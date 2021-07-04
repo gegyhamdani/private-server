1717a307b03406274f450e76028bd341850d6195
gegyhamdani <gegyhamdani@gmail.com>
2021-07-05 02:20:02 +0700

    change port

--- db.js
+++ db.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
 const Pool = new require("pg").Pool;
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_DATABASE,
+  ssl: isProduction
 });
 
 module.exports = pool;
