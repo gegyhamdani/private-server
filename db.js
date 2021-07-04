7fe61809f556b60e82277691d1e8aa5a76c956e8
gegyhamdani <gegyhamdani@gmail.com>
2021-07-05 02:22:00 +0700

    change db

--- db.js
+++ db.js
@@ -8,8 +8,7 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
-  ssl: isProduction
+  database: process.env.DB_DATABASE
 });
 
 module.exports = pool;
