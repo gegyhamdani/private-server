dcf4e43d401b70de71343a90c8618fc9043daf5d
gegyhamdani <gegyhamdani@gmail.com>
2021-07-05 02:17:29 +0700

    change config db

--- db.js
+++ db.js
@@ -1,13 +1,12 @@
-require('dotenv').config()
-
-const {Pool} = require('pg')
-const isProduction = process.env.NODE_ENV === 'production'
-
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+require("dotenv").config();
+const Pool = new require("pg").Pool;
 
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction,
-})
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+});
 
-module.exports = {pool}
+module.exports = pool;
